b88fd0c266089f6ad6ebb858369355b8635570d2
Alvaro Esparza <aesparza@concentrico.com.mx>
2018-06-12 13:15:47 -0500

    uso de ngOnInit

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'title',
 	templateUrl: 'title.component.html'
 })
-export class TitleComponent{
+export class TitleComponent implements OnInit{
 	public name:string = "Alvaro";
+	public edad:number;
+	private _value : string;
+	public edades:number [];
+
+	constructor(){}
+	
+	//se ejecuta cuando el componente ha sido inicializado
+	ngOnInit(){
+			this.name= "Alex";
+			this.edad=23;
+		}
+
+	getEdadNombre() : string{
+		return `${this.name} : ${this.edad}`;
+	}
 }
+
