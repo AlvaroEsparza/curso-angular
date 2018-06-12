fd77a46e28a9186651008efd3e2df85377e6b6cc
Alvaro Esparza <aesparza@concentrico.com.mx>
2018-06-12 12:40:18 -0500

    repatacion de eeror y eliminacion de archivo extra

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,9 +1,9 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'title',
-	template: `<h1>curso por Alvaro</h1>`,
-});
+	templateUrl: 'title.component.html'
+})
 export class TitleComponent{
 
 }
