3f07a99e279c569696f1345a361d32a4954d7e05
Alvaro Esparza <alvaro.aer7882@gmail.com>
2018-06-12 03:54:25 -0500

    agregue cambio

--- src/app/title.component.ts
+++ src/app/title.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
 	selector: 'title',
-	template: `<h1>curso por Alvaro</h1>`,
+	template: `<h1>curso por Alvaro Esparzs</h1>`,
 });
 export class TitleComponent{
 
