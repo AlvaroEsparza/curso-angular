d00935aa1c1fb582a4bbc0e0240b3a3403043067
Alvaro Esparza <aesparza@concentrico.com.mx>
2018-06-13 13:17:00 -0500

    ng-class diferentes formas de hacerlo,  string, arreglo y objeto de configuracion

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -13,13 +13,19 @@ export class TitleComponent implements OnInit{
 
 	public isAvailable : boolean = true;
 
+	public css_classes : string[] =['active', 'shadow'];
+
 	constructor(){}
 
 	//se ejecuta cuando el componente ha sido inicializado
 	ngOnInit(){
 			this.name= "Alex";
 			this.edad=23;
+			setTimeout(()=> this.name = "Alvaro Esparza Rivero", 3000);
+
 			setTimeout(()=> this.isAvailable = false, 3000);
+
+			setTimeout(()=> this.css_classes = ['shadow'], 3000);
 		}
 
 
